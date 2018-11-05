2d5fda24e8573f84b52728b56cc5f5e86a1af3d8
Kocsis Róbert <robertzkocsis@gmail.com>
2018-11-05 22:15:18 +0100

    Create gridBuilder function which currently creates a container div and appends it to the body

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+var body = document.getElementsByTagName("BODY")[0];
+
+function gridBuilder(){
+    let container = document.createElement("div");
+    container.id = "container";
+
+    body.appendChild(container);
+}
