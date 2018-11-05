02e4bd2f8fdec17e822575b8389ccd204b017013
Kocsis Róbert <robertzkocsis@gmail.com>
2018-11-05 22:17:02 +0100

    Rename gridBuilder to containerBuilder

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 var body = document.getElementsByTagName("BODY")[0];
 
-function gridBuilder(){
+function containerBuilder(){
     let container = document.createElement("div");
     container.id = "container";
 
     body.appendChild(container);
+
 }
+
